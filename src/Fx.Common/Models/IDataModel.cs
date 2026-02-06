431c3cdaa1a74b06c3631fabd84e89b40836ff0b
nima aran <aran.nima@outlook.com>
2026-02-06 12:30:36 +0330

    feat: A new interface for all kind of data models defined.

---
+++ src/Fx.Common/Models/IDataModel.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------------------------------------------------------
+// All rights reserved. This project and this file is published under the Apache License 2. So, Any
+// use of this project must comply with the terms and policies described in the project’s `LICENSE`
+// file. For more information, please visit the project repository on GitHub or contact with owner.
+// ------------------------------------------------------------------------------------------------
+
+namespace Fx.Common.Models;
+
+/// <summary>
+/// A marker interface used to introduce data models such as entities, aggregates, view models,
+/// DTOs, etc.
+/// </summary>
+public interface IDataModel;
