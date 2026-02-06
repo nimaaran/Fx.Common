e98d2ccd8478961c7a94f68526ae59ec02e5d21d
nima aran <aran.nima@outlook.com>
2026-02-06 19:59:10 +0330

    feat: added two interface for defining domain services and aggregate key generators.

---
+++ src/Fx.Common.Domains/Services/IAggregateKeyGenerator.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------------------------------------------
+// All rights reserved. This project and this file is published under the Apache License 2. So, Any
+// use of this project must comply with the terms and policies described in the project’s `LICENSE`
+// file. For more information, please visit the project repository on GitHub or contact with owner.
+// ------------------------------------------------------------------------------------------------
+
+using Fx.Common.Domains.Aggregates;
+using Fx.Common.Services;
+
+namespace Fx.Common.Domains.Services;
+
+/// <summary>
+/// A contract for aggregate key generators.
+/// </summary>
+/// <typeparam name="TKey">Type of the aggregate key.</typeparam>
+public interface IAggregateKeyGenerator<out TKey> : IDomainService, ISingletonService
+    where TKey : notnull, IAggregateKey
+{
+    /// <summary>
+    /// Generates a new aggregate key.
+    /// </summary>
+    /// <returns>A new aggregate key.</returns>
+    TKey Generate();
+}
---
+++ src/Fx.Common.Domains/Services/IDomainService.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------------------------------------------
+// All rights reserved. This project and this file is published under the Apache License 2. So, Any
+// use of this project must comply with the terms and policies described in the project’s `LICENSE`
+// file. For more information, please visit the project repository on GitHub or contact with owner.
+// ------------------------------------------------------------------------------------------------
+
+using Fx.Common.Services;
+
+namespace Fx.Common.Domains.Services;
+
+/// <summary>
+/// A marker interface used to identify domain services. Domain services should not be exposed
+/// outside the domain's boundaries.
+/// </summary>
+public interface IDomainService : IService;
