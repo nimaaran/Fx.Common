43715acce797cbbd05ddbca9856d3cf7dd2416c0
nima aran <aran.nima@outlook.com>
2026-02-16 20:34:32 +0330

    feat: two interfaces added to support domain policy implementation in domains.

---
+++ src/Fx.Common.Domains/Policies/IDomainPolicy.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------------------------
+// All rights reserved. This project and this file is published under the Apache License 2. So, Any
+// use of this project must comply with the terms and policies described in the project’s `LICENSE`
+// file. For more information, please visit the project repository on GitHub or contact with owner.
+// ------------------------------------------------------------------------------------------------
+
+using Fx.Common.Domains.Aggregates;
+
+namespace Fx.Common.Domains.Policies;
+
+#pragma warning disable S2326 // Unused type parameters should be removed.
+
+/// <summary>
+/// A marker interface used to introduce domain policies.
+/// </summary>
+public interface IDomainPolicy;
+
+/// <summary>
+/// A marker interface used to introduce aggregate-based domain policies.
+/// </summary>
+/// <typeparam name="TAggregateRoot">Type of the aggregate root.</typeparam>
+public interface IDomainPolicy<TAggregateRoot> : IDomainPolicy
+    where TAggregateRoot : IAggregateRoot;
+
+#pragma warning restore S2326 // Unused type parameters should be removed.
---
+++ src/Fx.Common.Domains/Policies/IDomainPolicyBinder.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------------------------------------------
+// All rights reserved. This project and this file is published under the Apache License 2. So, Any
+// use of this project must comply with the terms and policies described in the project’s `LICENSE`
+// file. For more information, please visit the project repository on GitHub or contact with owner.
+// ------------------------------------------------------------------------------------------------
+
+using Fx.Common.Domains.Aggregates;
+using Fx.Common.Services;
+
+namespace Fx.Common.Domains.Policies;
+
+/// <summary>
+/// A contract for aggregate-based domain policy binders.
+/// </summary>
+/// <typeparam name="TAggregateRoot">Type of the aggregate root.</typeparam>
+public interface IDomainPolicyBinder<in TAggregateRoot> : ISingletonService
+    where TAggregateRoot : class, IAggregateRoot
+{
+    /// <summary>
+    /// Binds a domain policy to an aggregate root object.
+    /// </summary>
+    /// <param name="instance">An instance of the aggregate root.</param>
+    void Bind(TAggregateRoot instance);
+}
